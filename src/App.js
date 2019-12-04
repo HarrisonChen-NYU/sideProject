3c4abf2affddaa1bac4a8901cefb4e4dd02f48fc
Harrison Chen <harrisonchen@Harrisons-MacBook-Air.local>
2019-12-04 09:54:03 +0800

    first checkpoint

--- src/App.js
+++ src/App.js
@@ -28,7 +28,10 @@ function App() {
 
 function Splash(){
   return(
-    <h1>Cash Flow</h1>
+  <div>
+    <br></br>
+      <h1>Cash Flow Application</h1>
+  </div>
   )
 } 
 
@@ -43,9 +46,8 @@ function Terms() {
 
     return(
       <div>
-        <h1> Agreements</h1>
         <div className="box">
-          <button onClick={handleClick}>Link to WeChat/Alipay</button>
+          <button class= "wechatBox" onClick={handleClick}>Link to WeChat/Alipay </button>
           {
             (click) ? (<div><p>You have Successfully Linked to Wechat/AliPay</p><Onboarding/></div>) : null
           }
@@ -65,6 +67,18 @@ function Onboarding(){
     console.log(localStorage.getItem('budget'));
   }
 
+  
+  const [tap,settap] = useState(false);
+  const handleClick = (e) => {
+    settap(true);
+  }
+  function hideContent(){
+    const hideTerms =Terms.isHidden
+    return(
+      hideContent()
+    )
+  }
+
   return(
   <div>
     <h3> What is your budget for the month?</h3>
@@ -73,7 +87,12 @@ function Onboarding(){
       name="budget"
       className= "Form-Control"
       type="number" />
-      <button type="submit" className="input box">SUBMIT</button>
+      <button type="submit" onClick={handleClick} className="input box">SUBMIT</button>
+      {
+         (tap) ? (<div><p>You have Successfully Set Up the App! </p> 
+         <hideContent/>
+         </div>) : null
+      }
     </form>
   </div>
   )
@@ -89,33 +108,74 @@ function Onboarding(){
 function Balance(props) {
 
   var Expenses = [
-    ["Friends", "Purse", "Tree", "Airplane Toy"],
-    ["Laptop", "Machine Gun", "Wallet", "Coding Software"],
-    ["Water", "Stocks", "Company", "Ice Cream"],
-    ["Cat", "Pop", "Hat", "Backpack"],
-
+    [{name: "Daily Expense", price: 10}],
+    [{name: "Daily Expense", price: 21}],
+    [{name: "Daily Expense", price: 12}],
+    [{name: "Daily Expense", price: 14}],
+    [{name: "Daily Expense", price: 15}],
+    [{name: "Daily Expense", price: 42}],
+    [{name: "Daily Expense", price: 38}],
+    [{name: "Daily Expense", price: 10}],
+    [{name: "Daily Expense", price: 21}],
+    [{name: "Daily Expense", price: 12}],
+    [{name: "Daily Expense", price: 14}],
+    [{name: "Daily Expense", price: 15}],
+    [{name: "Daily Expense", price: 42}],
+    [{name: "Daily Expense", price: 38}],
+    [{name: "Daily Expense", price: 10}],
+    [{name: "Daily Expense", price: 21}],
+    [{name: "Daily Expense", price: 12}],
+    [{name: "Daily Expense", price: 14}],
+    [{name: "Daily Expense", price: 15}],
+    [{name: "Daily Expense", price: 42}],
+    [{name: "Daily Expense", price: 38}],
+    [{name: "Daily Expense", price: 10}],
+    [{name: "Daily Expense", price: 21}],
+    [{name: "Daily Expense", price: 12}],
+    [{name: "Daily Expense", price: 14}],
+    [{name: "Daily Expense", price: 15}],
+    [{name: "Daily Expense", price: 42}],
+    [{name: "Daily Expense", price: 38}],
+    [{name: "Daily Expense", price: 10}],
+    [{name: "Daily Expense", price: 21}],
+    [{name: "Daily Expense", price: 12}],
+    [{name: "Daily Expense", price: 14}],
+    [{name: "Daily Expense", price: 15}],
   ]
+
 
   const budget =  localStorage.getItem('budget')
   const balance = budget/30
+
   const dayOfMonth = props.day.getDate();
   const day = dayOfMonth.toString()
   const month = props.day.toLocaleString('default', {month: 'long'});
 
-  const items = Expenses[day - 1]
-  
+  const items = Expenses[day - 1]  
+
+  let perDay = 0;
+
   const itemsMap = (items) ? items.map((item) => {
+
+
+    perDay += item.price;
+
     return (
-      <p>{item}</p>
+      <div>
+      <p>Item Name: {item.name}<br></br>Item Price: ${item.price}</p> <br></br>
+      </div>
     );
   }) : null
 
+
   return(
   <div> 
-    <label> Today is {month + ' ' + day}</label>
-    <h2> Your Balance today: {balance} </h2>
-    <h2> Expenses</h2>  
-    {(items) ? itemsMap : "You have no expenses yet"}
+    <label> <br></br> Today is {month + ' ' + day}</label>
+    <h2> Your Remaining Balance today: ${Math.round(balance) - 
+      Math.round(perDay)} </h2>
+      <h3> Expenses</h3>  
+    {(items) ? itemsMap : "You have no expenses yet"} 
+    <br></br>
   </div>
   )
 }
